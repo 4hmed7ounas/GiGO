0d103307496118a91a855d704191f9310cb8fb18
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-10-01 03:59:08 +0500

    fix: fix the options thing DB

--- app/(website)/profile/layout.tsx
+++ app/(website)/profile/layout.tsx
@@ -1,10 +1,15 @@
 "use client";
 import FreelancerNavbar from "@/app/components/header/freelancernavbar";
-import React from "react";
+import ClientNavbar from "@/app/components/header/clientnavbar"; // Import ClientNavbar
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/config";
 import { useRouter } from "next/navigation";
 import { signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
+const db = getFirestore(auth.app);
 
 export default function Layout({
   children,
@@ -12,7 +17,23 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   const [user] = useAuthState(auth);
+  const [role, setRole] = useState<string | null>(null);
   const router = useRouter();
+
+  useEffect(() => {
+    const checkUserRole = async () => {
+      if (user) {
+        const userDocRef = doc(db, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setRole(userData.role);
+        }
+      }
+    };
+
+    checkUserRole();
+  }, [user]);
 
   if (typeof window !== "undefined") {
     const userSession = sessionStorage.getItem("user");
@@ -29,7 +50,11 @@ export default function Layout({
   return (
     <div className="flex bg-white">
       <div>
-        <FreelancerNavbar onSignOut={handleSignOut} />
+        {role === "buyer" ? (
+          <ClientNavbar onSignOut={handleSignOut} />
+        ) : (
+          <FreelancerNavbar onSignOut={handleSignOut} />
+        )}
       </div>
       <div className="w-full md:w-1/2">{children}</div>
     </div>
