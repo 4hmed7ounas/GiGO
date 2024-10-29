4733b45e5529028abf8c629fb202868a8d019b6e
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-10-30 04:08:27 +0500

    fix: final touches

--- app/(webapp)/api/checkDatabasezaid/route.ts
+++ app/(webapp)/api/checkDatabasezaid/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const client = await clientPromise;
-    await client.db('hadeed').command({ ping: 1 }); // Ping the database to check the connection
+    await client.db('zaidsharazi').command({ ping: 1 }); // Ping the database to check the connection
     return NextResponse.json({ message: 'Database connected successfully!' });
   } catch (error) {
     console.error('Failed to connect to the database:', error);
