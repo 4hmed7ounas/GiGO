9d1f16b86b757f9e4d9380d2c7a5ca3f22fbf202
hawdeed <hadeeda980@gmail.com>
2024-11-28 04:58:34 +0500

    feat:made integerated sidebar of real time chat

--- app/(webapp)/realTimeChat/components/sidebar/index.tsx
+++ app/(webapp)/realTimeChat/components/sidebar/index.tsx
@@ -1,32 +1,25 @@
-"use client"; // Ensure this is the first line
+"use client";
 
 import Image from "next/image";
 import React from "react";
-
-interface Message {
-  id: number;
-  text: string;
-  sender: string;
-}
+import { IMAGES } from "../../../../../share/assets";
 
 interface User {
-  id: number;
-  sender: string;
-  avatarUrl?: string; // Optional avatar URL
-  messages: Message[]; // Add messages property here
+  id: string;
+  name: string;
 }
 
 interface SidebarProps {
   users: User[];
-  onUserClick: (user: User) => void; // Prop for handling user clicks
+  //onUserClick: (user: User) => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ users, onUserClick }) => {
-  const truncateTitle = (title: string, maxLength: number) => {
-    return title.length > maxLength
-      ? `${title.substring(0, maxLength)}...`
-      : title;
-  };
+const Sidebar: React.FC<SidebarProps> = ({ users}) => {
+  // const truncateTitle = (title: string, maxLength: number) => {
+  //   return title.length > maxLength
+  //     ? `${title.substring(0, maxLength)}...`
+  //     : title;
+  // };
 
   return (
     <div className="w-1/2 bg-gray-100 border-r border-gray-300 h-screen p-4 overflow-y-auto mt-16">
@@ -36,20 +29,18 @@ const Sidebar: React.FC<SidebarProps> = ({ users, onUserClick }) => {
           <div
             key={user.id}
             className="flex items-center space-x-3 cursor-pointer hover:bg-gray-200 p-2 rounded-lg transition-colors duration-200 ease-in-out"
-            onClick={() => onUserClick(user)}
+            // onClick={() => onUserClick(user)}
           >
             <Image
-              src={user.avatarUrl || "/default-avatar.jpg"}
-              alt={user.sender}
+              src={IMAGES.profile}
+              alt="profile"
               width={40}
               height={40}
               className="w-10 h-10 rounded-full border border-gray-300 shadow-sm"
             />
             <div className="flex-1">
-              <h3 className="font-medium text-gray-700">{user.sender}</h3>
-              <p className="text-sm text-gray-500 truncate">
-                {truncateTitle(user.messages[0]?.text, 30) || "No messages"}
-              </p>
+              <h3 className="font-medium text-gray-700">{user.name}</h3>
+              <p>{user.id}</p>
             </div>
           </div>
         ))}
